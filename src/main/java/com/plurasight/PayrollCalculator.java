5db64c9a6d25cdd6115f67e4abec30a640f00557
josem71411-gif <josem71411@gmail.com>
2026-04-10 10:14:04 -0500

    asking for employee name with the function to read full line and store it

--- src/main/java/com/plurasight/PayrollCalculator.java
+++ src/main/java/com/plurasight/PayrollCalculator.java
@@ -6,6 +6,10 @@ public class PayrollCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+// Ask for employee name
+        System.out.print("Enter employyee name: ");
+        String employeeName = input.nextLine();
+
 
     }
 }
