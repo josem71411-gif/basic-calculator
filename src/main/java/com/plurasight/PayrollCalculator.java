8be1a19bde6f02b1c46fccb28aa13d10a6ff72a4
josem71411-gif <josem71411@gmail.com>
2026-04-10 10:12:08 -0500

    Created scanner object

--- src/main/java/com/plurasight/PayrollCalculator.java
+++ src/main/java/com/plurasight/PayrollCalculator.java
@@ -1,4 +1,11 @@
 package com.plurasight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+    }
 }
